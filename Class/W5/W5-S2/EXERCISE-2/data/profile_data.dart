c4dc7e5798464e5ae5f0626b5bcf2371624f0c68
Hyunnin04 <160579920+Hyunnin04@users.noreply.github.com>
2024-11-17 20:54:36 +0700

    profile_data.dart

--- Class/W5/W5-S2/EXERCISE-2/data/profile_data.dart
+++ Class/W5/W5-S2/EXERCISE-2/data/profile_data.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:test_flutter/W5/W5-S2/EXERCISE-2/model/profile_tile_model.dart';
- 
+import '../model/profile_tile_model.dart';
+
 ProfileData ronanProfile = ProfileData(
-    name: "Ronan",
-    position: "Flutter Developer",
-    avatarUrl: 'assets/w5-s2/aang.png',
-    tiles: [
-      TileData( icon: Icons.phone, title: "Phone Number", value: "+123 456 7890"),
-      TileData(icon: Icons.location_on, title: "Address", value: "123 Cambodia"),
-      TileData(icon: Icons.email, title: "Mail", value: "ronan.ogogr@cadt.edu"),
-    ]);
+  name: "Ronan",
+  position: "Flutter Developer",
+  avatarUrl: 'assets/W5-S2/image.png',
+  tiles: [
+    TileData(icon: Icons.phone, title: "Phone Number", value: "+123 456 7890"),
+    TileData(icon: Icons.location_on, title: "Address", value: "123 Cambodia"),
+    TileData(icon: Icons.email, title: "Mail", value: "ronan.ogogr@cadt.edu"),
+  ],
+);
+
+ProfileData saraProfile = ProfileData(
+  name: "Danin",
+  position: "CS Student",
+  avatarUrl: 'assets/W5-S2/image2.png',
+  tiles: [
+    TileData(icon: Icons.phone, title: "Phone Number", value: "+987 654 3210"),
+    TileData(icon: Icons.location_on, title: "Address", value: "456 Cambodia"),
+    TileData(icon: Icons.email, title: "Mail", value: "ninnin@cadt.edu"),
+  ],
+);
+
+ProfileData jamesProfile = ProfileData(
+  name: "Holo",
+  position: "Backend Developer",
+  avatarUrl: 'assets/W5-S2/image3.png',
+  tiles: [
+    TileData(icon: Icons.phone, title: "Phone Number", value: "+112 233 4455"),
+    TileData(icon: Icons.location_on, title: "Address", value: "789 Cambodia"),
+    TileData(icon: Icons.email, title: "Mail", value: "Holo.Hee@cadt.edu"),
+  ],
+);
